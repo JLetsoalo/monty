6c1bdf9437ef873798b44b43212c3c2c03a97b10
Anqobile <Anqobilegylar@gmail.com>
2023-08-27 19:48:25 -0700

    monty

---
+++ tests/stack_pos.c
@@ -0,0 +1,85 @@
+/**
+ *  * swap - swap locations of previous stack with the top stack
+ *   * @h: node to be swapped
+ *    * @line_number: node number
+ *     */
+
+void swap(stack_t **h, unsigned int line_number)
+{
+	stack_t *tmpry = NULL;
+
+	if (*h == NULL || (*h)->next == NULL)
+	{
+		printf("L%u: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	tmpry = (*h)->next;
+	if (tmpry->next != NULL)
+	{
+		(*h)->next = tmpry->next;
+		(*h)->next->prev = *h;
+
+	}
+	else
+	{
+		tmpry->prev->prev = tmpry;
+		tmpry->prev->next = NULL;
+		tmpry->prev = NULL;
+		tmpry->next = *h;
+		(*h) = tmpry;
+	}
+}
+
+/**
+ *  * rotl - rotate so top of stack becomes last one, second becomes first one
+ *   * @h: node to be rotated
+ *    * @line_number: node number
+ *     */
+
+void rotl(stack_t **h, unsigned int line_number)
+{
+	stack_t *tmpry;
+
+	(void) line_number;
+
+	if ((*h)->next != NULL)
+	{
+		tmpry = *h;
+		while (tmpry->next != NULL)
+		{
+			tmpry = tmpry->next;
+		}
+		(*h)->prev = tmpry;
+		tmpry->next = *h;
+		(*h)->next->prev = NULL;
+		*h = (*h)->next;
+		tmpry->next->next = NULL;
+	}
+}
+
+/**
+ *  * rotr - rotate so only bottom node of stack becomes first one
+ *   * @h: node to be rotated
+ *    * @line_number: node number
+ *     */
+
+void rotr(stack_t **h, unsigned int line_number)
+{
+	stack_t *tmpry;
+
+	(void) line_number;
+
+	if ((*h)->next != NULL)
+	{
+		tmpry = *h;
+		while (tmpry->next != NULL)
+		{
+			tmpry = tmpry->next;
+			(*h)->prev = tmpry;
+			tmpry->next = *h;
+			tmpry->prev->next = NULL;
+			tmpry->prev = NULL;
+			(*h) = (*h)->prev;
+		}
+	}
+}
